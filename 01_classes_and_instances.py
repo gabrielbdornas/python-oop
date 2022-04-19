c78c2b1d436a176dd3b8f2346b90824d36aca27c
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-04-19 11:05:59 -0300

    notes

--- 01_classes_and_instances.py
+++ 01_classes_and_instances.py
@@ -3,9 +3,15 @@
 # Quase tudo visto até agora são classes
 
 # Pq utilizar classes ou objetos?
-  # Permite agrupar dados e funções/comportamentos de modo que a reutilização de código fica mais fácil
+  # Permite agrupar dados e comportamentos de modo que a reutilização de código fica mais fácil
   # data = atributos
-  # funções/comportamentos = métodos
+  # comportamentos = métodos
+
+class Employee:
+
+    def __init__(self, name, full_name):
+        self.name = name
+        self.full_name = full_name
 
 # Pense em uma classe como uma forma, que poderá ser utilizada para criação de vários objetos ao longo da exeução
 # de seu programa.
@@ -22,42 +28,6 @@
 
 # 1 - Cada instância criada será única
 
-""" class Employee:
-  pass
-
-empl_gabriel = Employee()
-empl_maria = Employee()
-empl_jose = Employee()
-
-# Únicos em diferentes locais na memória
-print(empl_gabriel)
-print(empl_maria)
-print(empl_jose) """
-
-# 1 - Cada instância criada será única
-
-# 2 -  Variáveis de instâncias são variáveis únicas para cada instância de uma classe
-
-""" empl_gabriel.first_name = 'Gabriel'
-empl_gabriel.last_name = 'Dornas'
-empl_gabriel.email = 'gabriel.dornas@company.com'
-empl_gabriel.pay = 50000
-
-empl_maria.first_name = 'Maria'
-empl_maria.last_name = 'José'
-empl_maria.email = 'maria.jose@company.com'
-empl_maria.pay = 60000
-
-empl_jose.first_name = 'José'
-empl_jose.last_name = 'Maria'
-empl_jose.email = 'jose.maria@company.com'
-empl_jose.pay = 40000
-
-print(empl_gabriel.first_name)
-print(empl_maria.first_name)
-print(empl_jose.first_name) """
-
-
 # 2 -  Variáveis de instâncias são variáveis únicas para cada instância de uma classe
 
 # 3 - Refatorando nosso código para criar variáveis de instância durante a criação da classe
@@ -67,65 +37,11 @@ print(empl_jose.first_name) """
   # __init__ recebe a instância automaticamente como primeiro argumento = self
   # Utilizar self = empl_gabriel.last_name, mas agora será feito automaticamente
 
-""" class Employee:
-
-  def __init__(self, first_name, last_name, pay):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = f'{first_name.lower()}.{last_name.lower()}@company.com'
-    self.pay = pay
-
-# Não será necessário utilizar self como primeiro argumento durante criação da instância
-# Não precisamos mais das instâncias e atributos definidas manualmente anteriormente
-empl_gabriel = Employee('Gabriel', 'Dornas', 50000)
-empl_maria = Employee('Maria', 'José', 60000)
-empl_jose = Employee('José', 'Maria', 40000)
-
-# Tudo continua funcionando
-print(empl_gabriel.email)
-print(empl_maria.email)
-print(empl_jose.email)
-
-print(f'{empl_gabriel.first_name} recebe R${empl_gabriel.pay}')
-print(f'{empl_maria.first_name} recebe R${empl_maria.pay}')
-print(f'{empl_jose.first_name} recebe R${empl_jose.pay}') """
-
-# 3 - Refatorando nosso código para criar variáveis de instância durante a criação da classe
-
 # 4 - Full Name: Nosso primeiro método de classe
   # Possível realizar manualmente
-
-""" class Employee:
-
-  def __init__(self, first_name, last_name, pay):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = f'{first_name.lower()}.{last_name.lower()}@company.com'
-    self.pay = pay
-
-  def full_name(self):
-    return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
-
-empl_gabriel = Employee('gabriel', 'dornas', 50000)
-empl_maria = Employee('maria', 'josé', 60000)
-empl_jose = Employee('josé', 'maria', 40000)
-
-# Não esqueça do parênteses após full_name pois isto é um método e não um atributo
-print(empl_gabriel.first_name)
-print(empl_maria.first_name)
-print(empl_jose.first_name) """
-
-""" print(empl_gabriel.full_name())
-print(empl_maria.full_name())
-print(empl_jose.full_name()) """
-
-# 4 - Full Name: Nosso primeiro método de classe
+  # Não esqueça do parênteses após full_name pois isto é um método e não um atributo
 
 # Cuidado: Não esqueça o primeiro argumento de métodos de classe - self
-
-# Print para mostrar que é a mesma coisa
-""" print(Employee.full_name(empl_gabriel)) # Aqui chamo o método pela classe e ele não sabe qual instância desejo
-print(empl_gabriel.full_name()) # Passa self automaticamente """
 
 # Resumo
   # Criamos nossa primeira classe
--- 03_exercises.py
+++ 03_exercises.py
@@ -15,19 +15,50 @@
 # Método Passou de ano (Retornar True ou False)
 # Notas de Português e Matemática Maior que 70
 
-# Método de Classe com a contagem dos alunos
+# Variável de Classe com a contagem dos alunos
 
 class Alunos:
 
-    def __init__(self, nome, sobrenome, idade, genero, nota_portugues, nota_matematica):
+    num_alunos = 0
+
+    def __init__(self, nome, sobrenome, idade, genero, nota_portugues, nota_matematica, nota_historia):
         self.nome = nome
         self.sobrenome = sobrenome
         self.idade = idade
         self.genero = genero
         self.nota_portugues = nota_portugues
         self.nota_matematica = nota_matematica
+        self.nota_historia = nota_historia
 
-aluno_1 = Alunos('Gabriel', 'Dornas', 37, 'Masculino', 70, 75)
+        Alunos.num_alunos +=1
 
-print(aluno_1.nome)
-print(aluno_1.nota_matematica)
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'
+
+    def add_nota(self, materia, nota):
+        if materia == 'Português':
+            self.nota_portugues += nota
+        elif materia == 'Matemática':
+            self.nota_matematica += nota
+        elif materia == 'História':
+            self.nota_historia += nota
+
+    def passou_de_ano(self):
+        if self.nota_matematica >= 70 and self.nota_portugues >= 70 and self.nota_historia >= 70:
+            return True
+        else:
+            return False
+
+aluno_1 = Alunos('Gabriel', 'Dornas', 37, 'Masculino', 68, 75, 80)
+aluno_1 = Alunos('Maria', 'José', 20, 'Feminino', 68, 75, 80)
+aluno_1 = Alunos('Maria', 'José', 20, 'Feminino', 68, 75, 80)
+aluno_1 = Alunos('Maria', 'José', 20, 'Feminino', 68, 75, 80)
+aluno_1 = Alunos('Maria', 'José', 20, 'Feminino', 68, 75, 80)
+
+""" print(aluno_1.nome)
+print(aluno_1.nota_portugues)
+aluno_1.add_nota('Português', 2)
+print(aluno_1.nota_portugues)
+print(aluno_1.nome_completo())
+print(aluno_1.passou_de_ano()) """
+print(Alunos.num_alunos)
